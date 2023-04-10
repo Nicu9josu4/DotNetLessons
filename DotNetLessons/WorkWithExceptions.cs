ab0ee37befcf4e17a522ee5c0551a630c717ede3
Josu Ion <i.josu@moldcell.md>
2023-04-10 09:21:09 +0300

    12

--- DotNetLessons/WorkWithExceptions.cs
+++ DotNetLessons/WorkWithExceptions.cs
@@ -14,21 +14,21 @@
             app.UseDeveloperExceptionPage(); // Catch an exception when Environment is Development
             app.UseStatusCodePages(); // Utilizarea paginilor pentru informarea utilizatorului despre status code si ce reprezinta ele.
                                       // Regula principala este ca ea sa fie amplasata inaintea la UseStaticFiles sau a oricarui Map
-            //app.UseStatusCodePagesWithRedirects("/error/{0}"); // Va transmite la pagina aratata dar cu status code deja gasit
-            //app.UseStatusCodePagesWithReExecute("/error/{0}"); // Va transmite la pagina cu exceptia dar cu status codul exceptiei
+                                      //app.UseStatusCodePagesWithRedirects("/error/{0}"); // Va transmite la pagina aratata dar cu status code deja gasit
+                                      //app.UseStatusCodePagesWithReExecute("/error/{0}"); // Va transmite la pagina cu exceptia dar cu status codul exceptiei
 
-            //if (!app.Environment.IsDevelopment())
-            //{
-            //    //app.UseExceptionHandler(app => app.Run(async context =>
-            //    //{
-            //    //    context.Response.StatusCode = 500;
-            //    //    await context.Response.WriteAsync("Error 500. DivideByZeroException occured!");
-            //    //}));
-            //}
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler(app => app.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    await context.Response.WriteAsync("Error 500. DivideByZeroException occured!");
+                }));
+            }
             app.Map("/error/{statusCode}", (int statusCode) => $"Error. Status Code: {statusCode}");
 
             /// Use Exception handler methods
-            app.MapGet("/getException", async context =>
+            app.Map("/getException", async context =>
             {
                 int a = 10;
                 int b = 0;
